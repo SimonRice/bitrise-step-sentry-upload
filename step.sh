2ca7ecf454dba60c39bebdc5a2550e9c3526fe2e
Mickey <knox@users.noreply.github.com>
2021-01-31 11:39:09 +0100

    Enable custom Sentry URL (#2)
    * allow custom sentry URL
    * allow `sentry_dsym_path` with whitespaces

--- step.sh
+++ step.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex
 
-sentry-cli --auth-token ${sentry_auth_token} upload-dif --org ${sentry_org_slug} --project ${sentry_project_slug} ${sentry_dsym_path}
+sentry-cli --auth-token ${sentry_auth_token} --url ${sentry_url} upload-dif --org ${sentry_org_slug} --project ${sentry_project_slug} "${sentry_dsym_path}"
